40dbb6159427cde941907830ce2cacf7fbec7abf
riddhi191203 <riddhijain.rj38.com>
2026-05-22 13:18:59 +0530

    minor changes for deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env
-      .VITE_API_URL,
+      .VITE_API_URL || "http://localhost:5000",
   withCredentials: true,
 
   headers: {
